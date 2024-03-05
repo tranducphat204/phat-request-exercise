c6a066a365d1f61698e789b1bc57dc2c9e5ad6a4
phatdeptraiii <vitphat350@gmail.com>
2024-03-05 08:49:46 +0700

    update

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -8,8 +8,8 @@ class FormController extends Controller
 {
     public function post(Request $request)
     {
-        return 'Name ' . ($request->input('username'))
+        return 'Name ' . ($request->input('name'))
             . '<br> UserName:  ' . ($request->input('username'))
-            . '<br> Password: ' . ($request->input('username'));
+            . '<br> Password: ' . ($request->input('password'));
     }
 }
